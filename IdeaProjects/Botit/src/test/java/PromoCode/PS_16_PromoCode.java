44d16ae2d7583878929ab98bee3cbdebc3536a94
mohamedesouki <mohamedshanab205@gmail.com>
2024-01-14 14:06:17 +0200

    second push

--- IdeaProjects/Botit/src/test/java/PromoCode/PS_16_PromoCode.java
+++ IdeaProjects/Botit/src/test/java/PromoCode/PS_16_PromoCode.java
@@ -1,24 +1,15 @@
 package PromoCode;
 
-import BaseWebsite.BasePromoCode;
-import BotitWebsite.Common_Methods;
 import ReadProductSheet.ReadPromoCode;
-import ReadProductSheet.ReadXLS;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class PS_16_PromoCode  {
+public class PS_16_PromoCode {
     WebDriver driver;
     public PageLoginPromo pageLoginPromo;
     public PagePromoCode pagePromoCode;
